9e20f1ccfb2178e6cdf48cd5256bc94807db04d6
SZJohanR4 <johan_szr@hotmail.com>
2018-11-01 22:40:00 -0500

    ver en detalle la receta y el producto

--- unimonito/producto/urls.py
+++ unimonito/producto/urls.py
@@ -6,4 +6,6 @@ app_name = 'producto'
 urlpatterns = [
     path('', views.pedidos, name='pedidos'),
     path('verifCliente', views.verifCliente, name='verifCliente'),
+    path('producto/<int:producto_id>', views.productoDetail, name='productoDetail'),
+    path('receta/<int:receta_id>', views.recetaDetail, name='recetaDetail'),
 ]
--- unimonito/producto/views.py
+++ unimonito/producto/views.py
@@ -23,7 +23,7 @@ def verifCliente(request):
         cedulaUsuario=datos.get("cedula_Usuario")
         ciudad=datos.get("ciudad_Usuario")
 
-        cliente=Clientes.objects.get(Cedula=cedulaUsuario)
+        cliente=Clientes.objects.filter(Cedula=cedulaUsuario)
         if not cliente:#el cliente ya ha comprado antes, debe mostrarse listado de productos que ha consumido, se muestra el formulario para pedir producto
             cliente=Clientes()
             cliente.Nombre=nombre
@@ -31,19 +31,25 @@ def verifCliente(request):
             cliente.Ciudad=ciudad
             cliente.save()
         else:#se agrega a la bd y se muestra el formulario para pedir producto
-            print("*******************************")
+            cliente=Clientes.objects.get(Cedula=cedulaUsuario)
             print(cliente)
-            recetasList=Recetas.objects.get(idClientes=cliente)
+            recetasList=Recetas.objects.filter(idClientes=cliente)
     productoList=Productos.objects.all()
-    if productoList:
-        print("hay productos")
-    if recetasList:
-        print("hay recetas")
     context = {
         'sesion':sesion,
         'productoList': productoList,
         'recetasList':recetasList,
         'getCliente':getClienteFORM
     }
-    template = loader.get_template('pedidos.html')
-    return HttpResponse(template.render(context, request))
+    return render(request,'pedidos.html',{"getCliente":getClienteFORM,"username":sesion,"productoList":productoList,"recetasList":recetasList})
+
+
+def productoDetail(request, producto_id):
+    sesion=request.user
+    producto = Productos.objects.get(pk=producto_id)
+    return render(request,'productoDetail.html',{"getCliente":getClienteFORM,"username":sesion,"producto":producto})
+
+def recetaDetail(request, receta_id):
+    sesion=request.user
+    receta = Recetas.objects.get(pk=receta_id)
+    return render(request,'recetaDetail.html',{"getCliente":getClienteFORM,"username":sesion,"receta":receta})
